1203cf1dda679e1edbcc1d719157ee8a417b7836
buwagaurav <gauravbuwa98@gmail.com>
2022-01-19 13:13:52 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-//console.log("Hi! This app is working.")
-var username = prompt("Give me your username")
-alert("this script works !! " + username)
+// After Button creation need to get the reference of that bubtton from html to js.
+//using document.querySelector
+
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
+console.log(txtInput);
+
+function clickHandler() {
+    console.log("clicked!");
+    console.log(txtInput.value);
+}
+
+// console.log(btnTranslate);
+// btnTranslate.addEventListener("click", function clickEventHandler(){
+//     console.log("Clicked");
+// })
+
+
+btnTranslate.addEventListener("click", clickHandler)
