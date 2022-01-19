7ddde6ac94c2e8068ce62485387aad604aa61a05
buwagaurav <gauravbuwa98@gmail.com>
2022-01-19 14:52:39 +0530

    input output processing done

--- app.js
+++ app.js
@@ -3,12 +3,18 @@
 
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+// outputDiv.innerText = "Gaurav Buwa"
 
 console.log(txtInput);
 
 function clickHandler() {
-    console.log("clicked!");
-    console.log(txtInput.value);
+    // console.log("clicked!");
+    // console.log(txtInput.value);
+    // console.log
+
+    outputDiv.innerText = "ajsjsjsjsjsjsj  " + txtInput.value;
 }
 
 // console.log(btnTranslate);
